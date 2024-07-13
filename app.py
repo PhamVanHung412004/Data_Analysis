0f6690c4509ed5f1a60e61f828c2355e1665ffb5
PhamVanHung412004 <hp36780@gmail.com>
2024-07-13 19:24:20 +0700

    mylovethuhien

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+
+# Đường dẫn đến tệp CSV của bạn
+csv_file_path = 'D:/Data_Analysis/dataset.csv'
+
+# Tiêu đề của ứng dụng
+st.title("Điểm thi tốt nghiệp năm 2022 trên địa bàn thành phố Hà Nội")
+
+
+# Đọc tệp CSV
+df = pd.read_csv(csv_file_path)
+df['Số báo danh'] = df['Số báo danh'].astype(str)
+df.insert(0, 'STT', range(1, len(df) + 1))
+
+# Hiển thị dataframe
+st.write("Dữ liệu từ tệp CSV:")
+st.dataframe(df)
+
+# Hiển thị thông tin bổ sung (tùy chọn)
+st.write("Thống kê tóm tắt:")
+st.write(df.describe())
+
+st.write("5 hàng đầu tiên:")
+st.write(df.head())
